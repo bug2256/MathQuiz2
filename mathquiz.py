9d3992d199c90e4969936e113ae527b4e3ad4613
bug2256 <33488824+bug2256@users.noreply.github.com>
2017-12-22 19:38:05 -0600

    not sure what changed.

--- mathquiz.py
+++ mathquiz.py
@@ -8,10 +8,12 @@ def say(something):
 
 #how big a number should we guess?
 max_number = 10
+max_scramble = 3
 first_line = "Guess a number between 1 and %d" % max_number
 print(first_line)
 say(first_line)
 number = randint(1, max_number)
+print(number)
 not_solved = True
 
 #keep looping unil we guess correctly
@@ -21,9 +23,11 @@ while not_solved:
     #say(you_said)
     if answer > number:
         print("the number is lower")
+        number = number - randint(1, max_scramble)
 #        say("The number is lower")
     elif answer < number:
         print("the number is higher")
+        number=number+randint(1, max_scramble)
 #        say("The number is higher")
     else:
         print("you got it right")
